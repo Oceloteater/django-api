63e48f8c18c37054ddcc587a6582fb862c4d6c32
Adam Carroll <Oceloteater@users.noreply.github.com>
2023-08-15 22:55:47 +0200

    Adding tags model serializer and tests

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -68,3 +68,9 @@ class ModelTests(TestCase):
             description='Sample recipe description'
         )
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful"""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+        self.assertEqual(str(tag), tag.name)
